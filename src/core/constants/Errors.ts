3db85e2ebb539e1c13d53fdaeb96445d7d088025
Carlos Alexandre <alexfilho062005@gmail.com>
2025-05-05 17:59:47 -0300

    :test_tube: test: create test validadin person name

--- src/core/constants/Errors.ts
+++ src/core/constants/Errors.ts
@@ -1,5 +1,10 @@
 const Errors = {
   EMPTY_NAME: 'Name cannot be empty',
+  LARGE_NAME: 'Name is too long',
+  SHORT_NAME: 'Name is too short',
+  NAME_WITHOUT_SURNAME: 'Name must contain a surname',
+  INVALID_CHARACTERS_NAME: 'Name contains invalid characters',
+
   INVALID_PASSWORD: 'Invalid password',
 } as const;
 
--- test/core/shared/person-name.test.ts
+++ test/core/shared/person-name.test.ts
@@ -4,3 +4,24 @@ import PersonName from '@/core/shared/person-name';
 test('Should throw error when trying to create an empty fname', () => {
   expect(() => new PersonName('')).toThrowError(Errors.EMPTY_NAME);
 });
+
+test('Should throw an error when creating a name shorter than 4 characters', () => {
+  expect(() => new PersonName('abc')).toThrowError(Errors.SHORT_NAME);
+});
+
+test('Should throw an error when creating a name more than 120 characters', () => {
+  const name = 'Pedro de Alcântara João Carlos de Bragança e Bourbon';
+  expect(() => new PersonName(name)).toThrowError(Errors.LARGE_NAME);
+});
+
+test('should throw an error when creating a first name without a last name', () => {
+  const name = 'Carlos';
+  expect(() => new PersonName(name)).toThrowError(Errors.NAME_WITHOUT_SURNAME);
+});
+
+test('should throw an error when creating a witch characteres especial', () => {
+  const name = 'Carlosdfar3252';
+  expect(() => new PersonName(name)).toThrowError(
+    Errors.INVALID_CHARACTERS_NAME,
+  );
+});
